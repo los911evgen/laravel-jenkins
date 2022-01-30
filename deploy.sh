c567380ab040fa0c6d9e0252a208dd1be4b20ab2
los911evgen <51365444+los911evgen@users.noreply.github.com>
2022-01-30 15:22:37 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 sed -i 's/latest/'$BUILD_NUMBER'/' docker-compose.yaml
 cat docker-compose.yaml
 cp env.docker .env
-ls
-#docker-compose up -d --force-recreate
+ls -lah
+docker-compose up -d --force-recreate
